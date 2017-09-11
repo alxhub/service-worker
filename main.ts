d7b95745e9b4a417e506210f82066901cf1fd429
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-09-11 11:11:51 -0700

    wip: working in the browser!

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-indexedDB.open('')
+import {Adapter} from './src/adapter';
+import {Driver} from './src/driver';
+import {CacheDatabase} from './src/db-cache';
+
+const scope = self as any as ServiceWorkerGlobalScope;
+
+const adapter = new Adapter();
+const driver = new Driver(scope, adapter, new CacheDatabase(scope, adapter));
---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: 'dist/main.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+  },
+};
